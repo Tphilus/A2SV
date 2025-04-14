043d852ce17a6b51bd246ab86fa851cf884541a5
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-04-14 03:31:00 -0700

    Created 'B - Average Numbers' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #7 14-Apr-2025/B - Average Numbers 333239.py
@@ -0,0 +1,17 @@
+# Problem: B - Average Numbers - https://codeforces.com/gym/602997/problem/B
+
+# Read input
+n = int(input())
+sequence = list(map(int, input().split()))
+
+
+def average_nums(n, sequence):
+    result = []
+    avg = sum(sequence) / len(sequence)
+
+    for i in range(n):
+        if sequence[i] == avg:
+            result.append(i + 1)
+    print(len(result))
+    print(*result)
+average_nums(n, sequence)
