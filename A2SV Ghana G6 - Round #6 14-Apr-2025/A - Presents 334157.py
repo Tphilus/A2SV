212f3461bbfc2f2d12567342280fd09c1e63e9ef
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-04-14 16:37:04 -0700

    Created 'A - Presents' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #6 14-Apr-2025/A - Presents 334157.py
@@ -0,0 +1,14 @@
+# Problem: A - Presents - https://codeforces.com/gym/601409/problem/A
+
+n = int(input())
+present_given = list(map(int, input().split()))
+
+result_list = [0] * n
+
+for i in range(n):
+    receiver = present_given[i]
+    result_list[receiver - 1] = i + 1
+
+print(*result_list)
+# ans = " ".join(map(str, result_list))
+# print(ans)
