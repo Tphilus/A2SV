52699b945e664bf458f600ef6d959285198f2418
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-04-14 04:02:33 -0700

    Created 'E - Coins' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #7 14-Apr-2025/E - Coins 333252.py
@@ -0,0 +1,10 @@
+# Problem: E - Coins - https://codeforces.com/gym/602997/problem/E
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+
+    def c_coins(n, k):
+        if n % 2 == 0 or (n - k) % 2 == 0 :
+            return "YES"
+        return "NO"
+    print(c_coins(n,k))
