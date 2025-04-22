2a348848edb4f27c2fe3489a07cb1581ad2f1731
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-04-22 04:51:42 -0700

    Created 'A - Marathon' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #8 Part 2 22-Apr-2025/A - Marathon 338529.py
@@ -0,0 +1,14 @@
+# Problem: A - Marathon - https://codeforces.com/gym/604840/problem/A
+
+t = int(input())
+for _ in range(t):
+    arr = list(map(int, input().split()))
+
+    def marathon(arr):
+        count = 0
+        for i in range(len(arr)):
+            if arr[i] > arr[0]:
+                count += 1
+        return count   
+
+    print(marathon(arr))
