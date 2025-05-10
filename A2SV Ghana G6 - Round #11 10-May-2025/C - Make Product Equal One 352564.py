59de7e777da4e0ac5056f8ffd11a258726e4d73b
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-05-10 15:23:06 -0700

    Created 'C - Make Product Equal One' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #11 10-May-2025/C - Make Product Equal One 352564.py
@@ -0,0 +1,30 @@
+# Problem: C - Make Product Equal One - https://codeforces.com/gym/609159/problem/C
+
+def min_count_to_make_product_one(n, arr):
+    count = 0
+    negative_count = 0
+    zero_count = 0
+
+    for num in arr:
+        if num > 1:
+            count += num - 1
+        elif num < -1:
+            count += abs(num + 1)
+            negative_count += 1
+        elif num == 0:
+            zero_count += 1
+            count += 1
+        elif num == -1:
+            negative_count += 1
+        else:  # num == 1, no count
+            continue
+
+    if negative_count % 2 == 1:
+        if zero_count == 0:
+            count += 2
+
+    return count
+
+n = int(input())
+arr = list(map(int, input().split()))
+print(min_count_to_make_product_one(n, arr))
