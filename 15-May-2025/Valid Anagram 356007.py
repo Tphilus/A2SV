d2777994f670c8e4d59d546dc8b27f4f61f73a96
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-05-14 22:59:41 -0700

    Created 'Valid Anagram' by A2SV Hub

---
+++ 15-May-2025/Valid Anagram 356007.py
@@ -0,0 +1,9 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        char_counter_s = Counter(s)
+        char_counter_t = Counter(t)
+
+        return char_counter_s == char_counter_t
