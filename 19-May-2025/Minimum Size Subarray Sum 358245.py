49f6ba807b8f6390aeffa7a1d5a2cad43a779c85
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2025-05-19 14:42:48 -0700

    Created 'Minimum Size Subarray Sum' by A2SV Hub

---
+++ 19-May-2025/Minimum Size Subarray Sum 358245.py
@@ -0,0 +1,20 @@
+# Problem: Minimum Size Subarray Sum - https://leetcode.com/problems/minimum-size-subarray-sum/
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        
+        left = 0
+        min_ = float("inf")
+        cur_sum = 0
+
+        for right in range(len(nums)):
+            cur_sum += nums[right]
+
+            while cur_sum >= target:
+                if right - left + 1 < min_:
+                    min_ = right - left + 1
+            
+                cur_sum -= nums[left]
+                left += 1
+        
+        return min_ if min_ != float("inf") else 0
